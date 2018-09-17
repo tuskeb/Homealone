28fc87db9c813fd531a67413b76fb0a028d3d939
tanulo <tuskebganz@gmail.com>
2018-09-17 15:56:29 +0200

    Szakkör, gyak

--- app/src/main/java/hu/csanyzeg/android/homealone/HouseViewFragment.java
+++ app/src/main/java/hu/csanyzeg/android/homealone/HouseViewFragment.java
@@ -4,10 +4,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,10 +21,17 @@ import hu.csanyzeg.android.homealone.Data.Data;
 public class HouseViewFragment extends SensorViewFragment {
 
 
+    TextView belsohom;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_house_view, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        belsohom = getView().findViewById(R.id.belsohomerseklet);
     }
 
     @Override
@@ -33,5 +42,12 @@ public class HouseViewFragment extends SensorViewFragment {
     @Override
     public void refreshUI(HashMap<String, Data> dataHashMap, ArrayList<Config> configs) {
 
+    }
+
+    @Override
+    public void setDatabaseService(DatabaseService databaseService) {
+        super.setDatabaseService(databaseService);
+        System.out.println(databaseService.getDataHashMap().get("C8").getConfig().display);
+        belsohom.setText(databaseService.getDataHashMap().get("C8").currentValue() + "");
     }
 }
